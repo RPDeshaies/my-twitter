a26806b1138977f0438c0195ad14a76c49d15bf5
rpdeshaies <rpdeshaies@gmail.com>
2022-01-14 10:05:17 -0500

    feat: initial

---
+++ src/domains/tweets/Tweets.ts
@@ -0,0 +1,23 @@
+export const Tweets: Record<string, Array<string>> = {
+  ["Charge RPG"]: [
+    `
+You want to start a new campaign, but you don't know which system to use? You should check-out Charge RPG! 
+
+It's free, open and developed by the Fari community. 
+
+Check it out below! https://fari-rpgs.itch.io/charge-rpg
+`,
+  ],
+  ["Newsletter"]: [
+    `
+Did you know I have a newsletter? Yeah that's right!
+
+Subscribe now to get notified when I release new games!
+
+https://www.getrevue.co/profile/fari-rpgs
+    `,
+  ],
+  ["Charge SRD"]: [],
+  ["The Path Of The Wolves"]: [],
+  ["HOPES & DREAMS"]: [],
+};
